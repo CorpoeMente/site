5c68de3f519732325c10e2821a24db45a07200cd
Joaoaalves <48325411+Joaoaalves@users.noreply.github.com>
2023-12-18 18:49:23 -0300

    Modificado o modelo de Departamento para incluir responsavel por ele

--- app/models/Departamento.js
+++ app/models/Departamento.js
@@ -14,6 +14,11 @@ const DepartamentoSchema = new mongoose.Schema(
       type: String,
       required: true,
     },
+    responsavel: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Profissional",
+      required: true,
+    },
   },
   { timestamps: true }
 );
